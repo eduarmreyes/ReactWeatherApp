ab7fc396b056242a2cc6f068665a35025b4ac138
eduarmreyes <eduar.mreyes@gmail.com>
2017-05-24 00:57:18 -0600

    Add Axios and Set action creator for fetchWeather

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,16 @@
-const API_KEY = "27b7625a4a1c6f9d96caf48ef499cddf";
+import axios from 'axios';
 
+const API_KEY = "27b7625a4a1c6f9d96caf48ef499cddf";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},de`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request,
+	}
+}
